86615a379dd4a31f76defae8ef6486d318781c2a
Uwe Voelker <uwe@uwevoelker.de>
2017-07-17 17:58:15 +0200

    margin factor

--- lib/BT/Sizing/IMFactor.pm
+++ lib/BT/Sizing/IMFactor.pm
@@ -2,7 +2,7 @@ package BT::Sizing::IMFactor;
 
 use Mojo::Base 'BT::Sizing';
 
-has 'im_factor';
+has [qw/im_factor margin_factor/];
 
 sub params {
     return [
@@ -13,6 +13,13 @@ sub params {
             type        => 'float',
             description => 'Keep ... times Initial Margin in cash',
         },
+        {
+            name        => 'margin_factor',
+            label       => 'Margin Factor',
+            default     => 1,
+            type        => 'float',
+            description => 'Model higher Margin Requirements',
+        },
     ];
 }
 
@@ -22,6 +29,8 @@ sub size {
     my $balance  = $arg{balance}  or die 'BALANCE missing';
     my $position = $arg{position} or die 'POSITION missing';
     my $margin   = $arg{margin}   or die 'MARGIN missing';
+
+    $margin *= $self->margin_factor;
 
     return int($balance / $margin / $self->im_factor);
 }
